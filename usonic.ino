795b1f1e8cea8f5ba6f014c188c6cf3a9e3c0a10
hicodepranav <52275444+hicodepranav@users.noreply.github.com>
2020-07-10 13:08:57 +0530

    Update usonic.ino

--- usonic.ino
+++ usonic.ino
@@ -1,4 +1,4 @@
-const int trigpin = 4;
+const int trigpin = 4;// u can put Vcc to 3.3V or 5V, it would work either way
 const int echopin = 5;
 #define led 13;
 long duration;
@@ -22,7 +22,7 @@ void loop() {
   Serial.print(distance);
   Serial.print("\n");
   delay(100);
-  if(distance<10)
+  if(distance<10)//if distance between ultra sonic sensor and object is <10, then Led pin 13 will glow, otherwise not. 
   {
     digitalWrite(13,HIGH);
   }
