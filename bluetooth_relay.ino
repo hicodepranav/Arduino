4b3f023611bbdd50737c9e05b7a10c497f91bcae
hicodepranav <52275444+hicodepranav@users.noreply.github.com>
2020-12-25 14:23:39 +0530

    Update bluetooth_relay.ino

--- bluetooth_relay.ino
+++ bluetooth_relay.ino
@@ -1,9 +1,9 @@
 void setup() {
 Serial.begin(9600);
-pinMode(6,OUTPUT);
-pinMode(7,OUTPUT);
-digitalWrite(6,HIGH);
-digitalWrite(7,HIGH);
+pinMode(6,OUTPUT); //relay 1 connected to D6
+pinMode(7,OUTPUT); //relay 2 connected to D7
+digitalWrite(6,HIGH); //initially relay 1 is OFF
+digitalWrite(7,HIGH); //initially relay 2 is OFF
 }
 
 void loop() {
@@ -15,7 +15,7 @@ void loop() {
     if(data=='1')
     {
     
-     digitalWrite(6, LOW);
+     digitalWrite(6, LOW);//relay 1 turns ON
 
     
       
@@ -23,15 +23,15 @@ void loop() {
     }
     else if(data=='2')
     {
-      digitalWrite(6, HIGH);
+      digitalWrite(6, HIGH); //relay 1 turns OFF
     }
         else if(data=='3')
     {
-      digitalWrite(7, LOW);
+      digitalWrite(7, LOW); //relay 2 turns ON
     }
         else if(data=='4')
     {
-      digitalWrite(7, HIGH);
+      digitalWrite(7, HIGH); //relay 2 turns OFF
     }
   }
 
